65615d34f14df2d4e40eed5d6e8b00e82a61ce8c
Thomas Vijverberg AC <tgmvijverberg@online.nl>
2017-09-04 17:18:30 +0200

    fix

--- RUNALL.m
+++ RUNALL.m
@@ -1,6 +1,12 @@
-% Load dataset to workspace
+%% Created by Thomas Vijverberg on 04-09-2017 at Radboud University Nijmegen
+% Last edited by Thomas Vijverberg on 04-09-2017
 
-disp('');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%OUTPUT%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Runs complete data analysis
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+% Load dataset to workspace
+% Skips load function if data is already in workspace
 if(exist('StempLobith') ~= 1)
     load('StempLobith.mat');
 end
@@ -11,5 +17,9 @@ if(exist('PNEC') ~= 1)
     load('PNEC.mat');
 end
 PNEC = fillPNEC_Table(PNEC);
-[ S_HighDensity ] = data_filter( StempLobith ,Slobith);
-[ S_HDODtox ] = toxicity_subr( S_HighDensity,PNEC );
+
+if(exist('S_HighDensity') ~= 1) %skip this step if we want to avoid long computation times
+    [ S_HighDensity ] = data_filter( StempLobith ,Slobith);
+end
+
+[ S_HighDensityToxicity ] = toxicity_subr( S_HighDensity,PNEC );
